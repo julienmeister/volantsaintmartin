a7dedb21d64e1e11e18ed6f91d49b39660e85b50
julienmeister <Julien.meister98@gmail.com>
2026-07-24 21:33:20 +0200

    test logo partner fix

--- src/app/features/informations/articles/partenaires-article/partenaires-article.ts
+++ src/app/features/informations/articles/partenaires-article/partenaires-article.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, inject } from '@angular/core';
 
 export const PARTENAIRES_INTRO =
   'Découvrez les structures et acteurs qui soutiennent le club, nos actions et le développement du badminton local.';
@@ -10,5 +11,13 @@ export const PARTENAIRES_INTRO =
   styleUrl: './partenaires-article.scss',
 })
 export class PartenairesArticle {
+  private readonly document = inject(DOCUMENT);
+
   readonly intro = PARTENAIRES_INTRO;
+  readonly subliteamLogoSrc = this.assetUrl('images/partners/subliteam.png');
+  readonly sportsRaquettesLogoSrc = this.assetUrl('images/partners/sports-raquettes.png');
+
+  private assetUrl(path: string): string {
+    return new URL(path, this.document.baseURI).toString();
+  }
 }
